a716c3fbcdf866fe9563c6ac3f51a9e17ecc4824
Ipatov <ikipatov11@gmail.com>
2024-10-24 15:28:43 +0300

    площадь круга

--- ConsoleAppGitTochlki/Program.cs
+++ ConsoleAppGitTochlki/Program.cs
@@ -13,7 +13,7 @@ class Program
         int y2 = random.Next(100);
         //две точки
         double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-
+        double dlinaokr = distance * 2;
         Console.WriteLine($"Точка 1: ({x1}, {y1})");
         Console.WriteLine($"Точка 2: ({x2}, {y2})");
         Console.WriteLine($"Расстояние между точками: {distance}");
